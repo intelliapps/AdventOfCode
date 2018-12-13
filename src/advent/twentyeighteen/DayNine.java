aa73a545c24d3d37036adc784bcccb0b8782c8cc
Vicki Clack <vicki.clack@intelliapps.co.uk>
2018-12-13 12:47:09 +0000

    Fixed DayNine to incorporate solution that satisfies part 2.

--- src/advent/twentyeighteen/DayNine.java
+++ src/advent/twentyeighteen/DayNine.java
@@ -1,5 +1,6 @@
 package advent.twentyeighteen;
 
+import advent.CircularDeque;
 import java.util.*;
 
 public class DayNine extends AdventOfCode
@@ -16,63 +17,26 @@ public class DayNine extends AdventOfCode
         super("", "");
     }
 
-    int partOneTwo(int numPlayers, int numMarbles)
+    long partOneTwo(int numPlayers, int numMarbles)
     {
-        Map<Integer, Integer> scores = new HashMap<>();
-        for (int i = 0; i <= numPlayers; i++) { scores.put(i, 0); }
-        List<Integer> marblesPlayed = new ArrayList<>();
-        marblesPlayed.add(0);
-        marblesPlayed.add(1);
+        CircularDeque<Integer> marblesPlayed = new CircularDeque<>();
+        marblesPlayed.addFirst(0);
+        long[] scores = new long[numPlayers];
 
-        int playerIndex = 2, index23 = 2, currentMarbleIndex = 1, currentInsertionIndex = 1;
-
-        for (int marbleNumber = 2; marbleNumber <= numMarbles; marbleNumber++)
+        for (int marbleNumber = 1; marbleNumber <= numMarbles; marbleNumber++)
         {
-            if (index23 == 23)
+            if (marbleNumber % 23 == 0)
             {
-                System.out.println(marbleNumber);
-                scores.put(playerIndex, scores.get(playerIndex) + marbleNumber);
-
-                currentMarbleIndex = currentMarbleIndex - 7;
-                if (currentMarbleIndex < 0) { currentMarbleIndex = marblesPlayed.size() + currentMarbleIndex; }
-                scores.put(playerIndex, scores.get(playerIndex) + marblesPlayed.get(currentMarbleIndex));
-                marblesPlayed.remove(currentMarbleIndex);
-                if (currentMarbleIndex == marblesPlayed.size()) { currentMarbleIndex = 0; }
-
-                if (currentMarbleIndex == marblesPlayed.size()-1)
-                {
-                    currentInsertionIndex = 1;
-                }
-                else
-                {
-                    currentInsertionIndex = currentMarbleIndex + 2;
-                }
-
-                index23 = 1;
+                marblesPlayed.rotate(-7);
+                scores[marbleNumber % numPlayers] += marbleNumber + marblesPlayed.pop();
             }
             else
             {
-                currentMarbleIndex = currentInsertionIndex;
-
-                if (currentInsertionIndex == marblesPlayed.size())
-                {
-                    marblesPlayed.add(marbleNumber);
-                    currentInsertionIndex = 1;
-                }
-                else
-                {
-                    marblesPlayed.add(currentInsertionIndex, marbleNumber);
-                    currentInsertionIndex += 2;
-                }
-
-                index23++;
+                marblesPlayed.rotate(2);
+                marblesPlayed.addLast(marbleNumber);
             }
-
-//            System.out.println("[" + playerIndex + "] (" + marblesPlayed.get(currentMarbleIndex) + ") " + marblesPlayed.toString());
-
-            playerIndex = playerIndex < numPlayers ? playerIndex + 1 : 1;
         }
 
-        return scores.values().stream().max(Comparator.comparing(Integer::intValue)).orElse(0);
+        return Arrays.stream(scores).max().orElse(0);
     }
 }
