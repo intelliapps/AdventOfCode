6f8f754449ad72aa91766397547da59a1dfabaca
Vicki Clack <vicki.clack@intelliapps.co.uk>
2019-12-06 15:59:23 +0000

    Add support for op codes 3 and 4 to IntCodeComputer

--- src/advent/twentynineteen/IntCodeComputer.java
+++ src/advent/twentynineteen/IntCodeComputer.java
@@ -1,15 +1,26 @@
 package advent.twentynineteen;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class IntCodeComputer
 {
     private int[] program;
+    private int[] inputs;
+    private List<Integer> outputs;
     private int instructionPointer;
 
     IntCodeComputer(String[] instructions)
     {
+        this(instructions, new int[]{});
+    }
+
+    IntCodeComputer(String[] instructions, int[] inputs)
+    {
         program = new int[instructions.length];
         for (int index = 0; index < instructions.length; index++) { program[index] = Integer.parseInt(instructions[index]); }
-        instructionPointer = 0;
+        this.inputs = inputs;
     }
 
     int runProgram(int noun, int verb)
@@ -21,12 +32,42 @@ public class IntCodeComputer
 
     int runProgram()
     {
-        do { instructionPointer = execTwoOperandInstruction(); }
+        return runProgram(new int[]{});
+    }
+
+    private int runProgram(int[] inputs)
+    {
+        outputs = new ArrayList<>();
+        instructionPointer = 0;
+        int opCode;
+        int inputNo = 0;
+
+        do
+        {
+            opCode = program[instructionPointer];
+
+            switch(opCode)
+            {
+                case 1: case 2:
+                    execTwoOperandInstruction();
+                    instructionPointer += 4;
+                    break;
+                case 3:
+                    execSaveInstruction(inputs[inputNo++]);
+                    instructionPointer += 2;
+                    break;
+                case 4:
+                    execOutputInstruction();
+                    instructionPointer += 2;
+                    break;
+            }
+        }
         while (program[instructionPointer] != 99);
+
         return program[0];
     }
 
-    private int execTwoOperandInstruction()
+    private void execTwoOperandInstruction()
     {
         int opCode = program[instructionPointer];
         int paramOneIndex = program[instructionPointer+1];
@@ -38,7 +79,17 @@ public class IntCodeComputer
             case 1: program[resultIndex] = program[paramOneIndex] + program[paramTwoIndex]; break;
             case 2: program[resultIndex] = program[paramOneIndex] * program[paramTwoIndex]; break;
         }
+    }
 
-        return instructionPointer + 4;
+    private void execSaveInstruction(int input)
+    {
+        int paramIndex = program[instructionPointer+1];
+        program[paramIndex] = input;
+    }
+
+    private void execOutputInstruction()
+    {
+        int paramIndex = program[instructionPointer+1];
+        outputs.add(program[paramIndex]);
     }
 }
